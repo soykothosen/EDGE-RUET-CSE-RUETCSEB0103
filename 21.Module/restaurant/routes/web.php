266e9e29b73b266c9c749c718534e007d8c95a23
Soykot Hosen <soykothosen1603028@gmail.com>
2025-01-12 19:12:14 +0600

    static file & component work properly

---
+++ 21.Module/restaurant/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/laravel', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
